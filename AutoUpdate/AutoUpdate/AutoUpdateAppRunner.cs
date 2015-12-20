fd9fe1318bfb1bf172cb790b55a3e68591cd7ff3
Mariusz Bojkowski <mariusz.bojkowski@gmail.com>
2015-12-20 14:33:27 +0100

    Execute application handling

--- AutoUpdate/AutoUpdate/AutoUpdateAppRunner.cs
+++ AutoUpdate/AutoUpdate/AutoUpdateAppRunner.cs
@@ -3,24 +3,76 @@ using AutoUpdate.Repo;
 using AutoUpdate.Updater;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace AutoUpdate
 {
     public class AutoUpdateAppRunner
     {
+        private readonly ApplicationRepository _repo;
+
         public AutoUpdateAppRunner(string blobUrl)
         {
-            var appRepo = new ApplicationRepository();
-            var appUpdater = new ApplicationUpdater(appRepo, blobUrl);
+            _repo = new ApplicationRepository();
+            var appUpdater = new ApplicationUpdater(_repo, blobUrl);
             appUpdater.Run();
         }
 
         public void Run(string[] args)
         {
-            System.Threading.Thread.Sleep(-1);
+            WaitForAppAvailability();
+            var dir = _repo.GenerateExecutiveArea();
+            var type = FindExecutive(dir);
+
+            var app = (IAutoUpdateApplication)Activator.CreateInstance(type);
+            app.Start(args);
+        }
+
+        private Type FindExecutive(string dir)
+        {
+            var apps = new List<Type>();
+            var libs = Directory.GetFiles(dir, "*.dll");
+            foreach(var lib in libs)
+            {
+                var types = InspectLibraryForExecutable(lib);
+                apps.AddRange(types);
+            }
+
+            return apps.First();
+        }
+
+        private IEnumerable<Type> InspectLibraryForExecutable(string path)
+        {
+            var apps = new List<Type>();
+            var asm = Assembly.LoadFile(path);
+            foreach (var type in asm.GetTypes())
+            {
+                if (!type.IsInterface)
+                {
+                    var interfaces = type.GetInterfaces();
+                    var autoUpdateInterfaces =
+                        interfaces.Where(i => i.Name == "IAutoUpdateApplication");
+                    if (autoUpdateInterfaces.Count() > 0)
+                    {
+                        apps.Add(type);
+                    }
+                }
+            }
+
+            return apps;
+        }
+
+        private void WaitForAppAvailability()
+        {
+            while (!_repo.HasData)
+            {
+                Thread.Sleep(100);
+            }
         }
     }
 }
--- AutoUpdate/AutoUpdate/Repo/ApplicationRepository.cs
+++ AutoUpdate/AutoUpdate/Repo/ApplicationRepository.cs
@@ -10,12 +10,28 @@ namespace AutoUpdate.Repo
     internal class ApplicationRepository
     {
         private readonly string _dataPath;
+        private readonly object _locker = new object();
 
         public DateTime ApplicationTimeStamp
         {
             get
             {
-                return Settings.Default.RepoTime;
+                lock (_locker)
+                {
+                    return Settings.Default.RepoTime;
+                }
+            }
+        }
+
+        public bool HasData
+        {
+            get
+            {
+                lock (_locker)
+                {
+                    ExistenceCheck();
+                    return Settings.Default.HasData;
+                }
             }
         }
 
@@ -41,21 +57,59 @@ namespace AutoUpdate.Repo
 
         public void Clear()
         {
-            Settings.Default.HasData = false;
-            Settings.Default.RepoTime = DateTime.MinValue;
-            Settings.Default.Save();
-
-            if (Directory.Exists(_dataPath))
+            lock (_locker)
             {
-                Directory.Delete(_dataPath, true);
+                Settings.Default.HasData = false;
+                Settings.Default.RepoTime = DateTime.MinValue;
+                Settings.Default.Save();
+
+                if (Directory.Exists(_dataPath))
+                {
+                    Directory.Delete(_dataPath, true);
+                }
+            }
+        }
+
+        public string GenerateExecutiveArea()
+        {
+            lock (_locker)
+            {
+                var dir = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+                if (!Directory.Exists(dir))
+                {
+                    Directory.CreateDirectory(dir);
+                }
+
+                var files = Directory.GetFiles(_dataPath);
+                foreach(var file in files)
+                {
+                    var fileName = Path.GetFileName(file);
+                    var targetPath = Path.Combine(dir, fileName);
+                    File.Copy(file, targetPath);
+                }
+
+                return dir;
             }
         }
 
         public void SetHadData(DateTime timeStamp)
         {
-            Settings.Default.RepoTime = timeStamp;
-            Settings.Default.HasData = true;
-            Settings.Default.Save();
+            lock (_locker)
+            {
+                Settings.Default.RepoTime = timeStamp;
+                Settings.Default.HasData = true;
+                Settings.Default.Save();
+            }
+        }
+
+        private void ExistenceCheck()
+        {
+            if (!Directory.Exists(_dataPath))
+            {
+                Settings.Default.HasData = false;
+                Settings.Default.RepoTime = DateTime.MinValue;
+                Settings.Default.Save();
+            }
         }
     }
 }
